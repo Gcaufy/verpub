a54db53438ce55e93be9daefec22cc2a89ba9ada
Gcaufy <gcaufy@gmail.com>
2019-10-16 10:57:02 +0800

    feat: added eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es6: true
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+};
